37fa2eff930b47024dc6ab4c9f4a074ae2f3fafc
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-04 01:01:26 -0500

    changed "integer" to "number" in comment

--- src/problems/multiples_of_3_or_5.rs
+++ src/problems/multiples_of_3_or_5.rs
@@ -4,7 +4,7 @@ pub const PROBLEM_ID: u16 = 1;
 pub fn run() -> u32 {
     let mut result: u32 = 0;
 
-    for i in 1..1000 { // for all integers between 0 and 1000
+    for i in 1..1000 { // for all numbers between 0 and 1000
         if [3, 5].iter().any(|x| i % x == 0) { // when either 3 or 5 are divisible by i
             result += i; // add i to the result
         }
