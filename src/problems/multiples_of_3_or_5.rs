fcf99d4437787e07a6ac897bc6e5aa0320b97255
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-04 00:47:54 -0500

    changed return type and added comments

--- src/problems/multiples_of_3_or_5.rs
+++ src/problems/multiples_of_3_or_5.rs
@@ -1,11 +1,12 @@
+// https://projecteuler.net/problem=1
 pub const PROBLEM_ID: u16 = 1;
 
-pub fn run() -> i32 {
-    let mut result: i32 = 0;
+pub fn run() -> u32 {
+    let mut result: u32 = 0;
 
-    for i in 0..1000 {
-        if [3, 5].iter().any(|x| i % x == 0) {
-            result += i;
+    for i in 1..1000 { // for all integers between 0 and 1000
+        if [3, 5].iter().any(|x| i % x == 0) { // when either 3 or 5 are divisible by i
+            result += i; // add i to the result
         }
     }
 
