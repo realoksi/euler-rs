48efed5c36507c326c083dd9f47b817cbf22be4b
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-04 22:05:50 -0500

    inserted problem runs into map, looping over print

--- src/problems.rs
+++ src/problems.rs
@@ -4,32 +4,22 @@ pub mod largest_prime_factor;
 pub mod multiples_of_3_or_5;
 pub mod smallest_multiple;
 
-// TODO: Implement a more automated method for running each problem.
+use std::collections::HashMap;
 
 pub fn run() {
-    println!(
-        "{}. {}",
-        multiples_of_3_or_5::PROBLEM_ID,
-        multiples_of_3_or_5::run()
-    );
-    println!(
-        "{}. {}",
-        even_fibonacci_numbers::PROBLEM_ID,
-        even_fibonacci_numbers::run()
-    );
-    println!(
-        "{}. {}",
-        largest_prime_factor::PROBLEM_ID,
-        largest_prime_factor::run()
-    );
-    println!(
-        "{}. {}",
-        largest_palindrome_product::PROBLEM_ID,
-        largest_palindrome_product::run()
-    );
-    println!(
-        "{}. {}",
-        smallest_multiple::PROBLEM_ID,
-        smallest_multiple::run()
-    );
+    let mut map: HashMap<u16, u64> = HashMap::new();
+
+    map.insert(multiples_of_3_or_5::PROBLEM_ID, multiples_of_3_or_5::run());
+    map.insert(even_fibonacci_numbers::PROBLEM_ID, even_fibonacci_numbers::run());
+    map.insert(largest_prime_factor::PROBLEM_ID, largest_prime_factor::run());
+    map.insert(largest_palindrome_product::PROBLEM_ID, largest_palindrome_product::run());
+    map.insert(smallest_multiple::PROBLEM_ID, smallest_multiple::run());
+
+    for i in map {
+        println!(
+            "{}. {}",
+            i.0 , i.1
+        );
+    }
+
 }
