a81a0d373dc084e4cd6d2110203268aaf107451f
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-06 13:27:33 -0500

    switched back return types to optimal size

--- src/problems/smallest_multiple.rs
+++ src/problems/smallest_multiple.rs
@@ -1,8 +1,8 @@
 // https://projecteuler.net/problem=5
 pub const PROBLEM_ID: u16 = 5;
 
-pub fn run() -> u64 {
-    let mut multiple: u64 = 20;
+pub fn run() -> u32 {
+    let mut multiple: u32 = 20;
 
     loop {
         if (1..=20).all(|x| -> bool { return multiple % x == 0 }) {
