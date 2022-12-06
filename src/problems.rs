b5392b246638427839db0b3b770d89bf5463ca15
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-06 13:28:44 -0500

    cargo formatted

--- src/problems.rs
+++ src/problems.rs
@@ -6,10 +6,34 @@ pub mod smallest_multiple;
 pub mod sum_square_difference;
 
 pub fn run() {
-    println!("{}. {}", multiples_of_3_or_5::PROBLEM_ID, multiples_of_3_or_5::run());
-    println!("{}. {}", even_fibonacci_numbers::PROBLEM_ID, even_fibonacci_numbers::run());
-    println!("{}. {}", largest_prime_factor::PROBLEM_ID, largest_prime_factor::run());
-    println!("{}. {}", largest_palindrome_product::PROBLEM_ID, largest_palindrome_product::run());
-    println!("{}. {}", smallest_multiple::PROBLEM_ID, smallest_multiple::run());
-    println!("{}. {:?}", sum_square_difference::PROBLEM_ID, sum_square_difference::run());
+    println!(
+        "{}. {}",
+        multiples_of_3_or_5::PROBLEM_ID,
+        multiples_of_3_or_5::run()
+    );
+    println!(
+        "{}. {}",
+        even_fibonacci_numbers::PROBLEM_ID,
+        even_fibonacci_numbers::run()
+    );
+    println!(
+        "{}. {}",
+        largest_prime_factor::PROBLEM_ID,
+        largest_prime_factor::run()
+    );
+    println!(
+        "{}. {}",
+        largest_palindrome_product::PROBLEM_ID,
+        largest_palindrome_product::run()
+    );
+    println!(
+        "{}. {}",
+        smallest_multiple::PROBLEM_ID,
+        smallest_multiple::run()
+    );
+    println!(
+        "{}. {:?}",
+        sum_square_difference::PROBLEM_ID,
+        sum_square_difference::run()
+    );
 }
--- src/problems/sum_square_difference.rs
+++ src/problems/sum_square_difference.rs
@@ -3,8 +3,8 @@ pub const PROBLEM_ID: u16 = 6;
 
 pub fn run() -> (u32, u32) {
     let range = 1u32..=100u32;
-    let range_sum : u32 = range.sum();
-    let range_pow : u32 = range_sum.pow(2);
+    let range_sum: u32 = range.sum();
+    let range_pow: u32 = range_sum.pow(2);
 
     (range_pow - range_sum, range_pow)
 }
