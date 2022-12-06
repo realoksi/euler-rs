d99e91fed276bc562e147680ecd57d271829e459
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-06 13:25:48 -0500

    added solution 6

---
+++ src/problems/sum_square_difference.rs
@@ -0,0 +1,10 @@
+// https://projecteuler.net/problem=6
+pub const PROBLEM_ID: u16 = 6;
+
+pub fn run() -> (u32, u32) {
+    let range = 1u32..=100u32;
+    let range_sum : u32 = range.sum();
+    let range_pow : u32 = range_sum.pow(2);
+
+    (range_pow - range_sum, range_pow)
+}
