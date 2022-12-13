e36d725db1c83ba5d6d4dd4b85c3db6a974f3aa0
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-12 19:34:16 -0500

    refactored

--- src/problems/sum_square_difference.rs
+++ src/problems/sum_square_difference.rs
@@ -1,21 +1,13 @@
 // https://projecteuler.net/problem=6
 pub const PROBLEM_ID: u16 = 6;
 
-// TODO: The following can be done way better.
-
 pub fn run() -> u64 {
-    let range = 1u64..=100u64;
-
+    const RANGE : std::ops::RangeInclusive<u64> = 1..=100;
+    
     let mut sum_square: u64 = 0;
-
-    range.clone().all( |i| {
+    for i in RANGE.clone() {
         sum_square += i.pow(2);
-        true
-    });
+    }
 
-    let range_sum: u64 = range.sum();
-
-    let square_sum: u64 = range_sum.pow(2);
-
-    square_sum - sum_square
+    RANGE.sum::<u64>().pow(2) - sum_square
 }
